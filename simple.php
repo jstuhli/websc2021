8e4e44a6b35cccc9f0fecdd864266d5a31388a94
jstuhli <josip.stuhli@gmail.com>
2021-08-11 12:31:06 +0200

    add cache

--- simple.php
+++ simple.php
@@ -1,12 +1,26 @@
 <?php
 header("Cache-Control: public, max-age=3600");
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <style>body {background-color: #<?php echo  rand(100000,999999);?></style>
-</head>
-<body>
-<a href="/">Reload</a>
-</body>
-</html>
+
+$num = 5;
+if($result = get_cache("calculate" . $num) === null) {
+    $result = calculate($num);
+    set_cache("calculate".$num, $result);
+}
+echo $result;
+
+function calculate($num) {
+    sleep(5);
+    return $num*2;
+}
+
+// issues
+// - cache never expires
+// - null is a legitimate value sometimes
+function get_cache($key) {
+    $content = @file_get_contents("cache/$key");
+    return $content !== false ? $content : null;
+}
+
+function set_cache($key, $value) {
+    return file_put_contents("cache/$key", $value);
+}
