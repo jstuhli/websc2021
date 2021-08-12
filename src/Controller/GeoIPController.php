ec30f1914eeb240ccdddf351677778e16e7be4a2
jstuhli <josip.stuhli@gmail.com>
2021-08-12 16:08:33 +0200

    add cache kernel

--- src/Controller/GeoIPController.php
+++ src/Controller/GeoIPController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\GeoIP\GeoIPService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class GeoIPController extends AbstractController
 {
     #[Route('/geoip/resolve/{ip}', name: 'geoip_resolve')]
+    #[Cache(maxage: 30, public: true)]
     public function resolve(GeoIPService $geoIP, ?string $ip = null): Response {
         $myIp = $ip ?? file_get_contents('https://api.ipify.org');
 
